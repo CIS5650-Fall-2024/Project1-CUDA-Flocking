263b38ea2930b9767c81afacba138ea38ca8700f
Yuhan Liu <02yuhanliu@gmail.com>
2024-09-07 19:06:25 -0400

    add images

--- src/kernel.cu
+++ src/kernel.cu
@@ -40,7 +40,7 @@ void checkCUDAError(const char *msg, int line = -1) {
 *****************/
 
 /*! Block size used for CUDA kernel launch. */
-#define blockSize 128
+#define blockSize 256
 
 // LOOK-1.2 Parameters for the boids algorithm.
 // These worked well in our reference implementation.
